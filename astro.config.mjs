a41c5df81e083e81abde4f92f59aebf17c930bc1
Steele Stonick <scstonick@gmail.com>
2026-02-26 14:17:05 -0500

    .com to .io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://swamppay.com',
+  site: 'https://swamppay.io',
   vite: {
     plugins: [tailwindcss()],
     server: {
